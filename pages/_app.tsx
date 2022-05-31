f5aa33f419271e0fa308828e6fab36baba6a105d
yuding2019 <yu_ding2019@outlook.com>
2022-05-31 22:37:43 +0800

    配置mdx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,32 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import styles from './_app.module.scss';
 import '../styles/styles.scss';
-import { useRouter } from 'next/router';
 
-function App({ Component, pageProps }) {
+const MDX_PAGE_COMPONENT_NAME = 'MDXContent';
+
+const App = ({ Component, pageProps }) => {
   const router = useRouter();
-  
+
   useEffect(() => {
     document.title = 'woooooh';
   }, []);
+
+  const pageComponent = Component.name === MDX_PAGE_COMPONENT_NAME
+    ? <Component {...pageProps} components={{ h2: () => 2333 }} />
+    : <Component {...pageProps} />;
 
   return (
     <section className={styles.wrap}>
       <header className={styles.header}>{router.pathname}</header>
       <div className={styles.contentWrap}>
         <div className={styles.content}>
-          <Component {...pageProps} />
+          {pageComponent}
         </div>
       </div>
     </section>
   )
-}
+};
 
 export default App;
