a6e96a6b7d9613a10a1e362330cb20e847b9cae8
yuding2019 <yu_ding2019@outlook.com>
2022-06-26 19:18:04 +0800

    自动创建笔记mdx文件

---
+++ scripts/createNote.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const dayjs = require('dayjs');
+
+const currentMonth = dayjs().format('YYYYMM');
+
+const files = fs.readdirSync(path.resolve(process.cwd(), 'pages/note'));
+
+const sameMonthFilesCount = files.filter((file) => file.startsWith(currentMonth)).length;
+const newNoteNumber = sameMonthFilesCount + 1;
+const newNoteMdxFileName = `${currentMonth}${newNoteNumber > 10 ? newNoteNumber : '0' + newNoteNumber}.mdx`;
+
+const initMdxContent = [
+  "import MDXPageLayout from '../../components/MDXCompoents';",
+  "",
+  "export const meta = {",
+  "  title: ''",
+  "};",
+  "",
+  "",
+  "",
+  "export default ({ children }) => <MDXPageLayout>{children}</MDXPageLayout>;",
+  "",
+].join('\n');
+
+fs.writeFileSync(path.resolve(process.cwd(), `pages/note/${newNoteMdxFileName}`), initMdxContent)
