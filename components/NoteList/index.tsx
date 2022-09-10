0cedbd623c12a8c90b6d3f13dc71e2247c5804e9
yuding2019 <yu_ding2019@outlook.com>
2022-09-10 19:29:57 +0800

    修改图片路径

--- components/NoteList/index.tsx
+++ components/NoteList/index.tsx
@@ -15,7 +15,7 @@ export interface NoteListProps {
 }
 
 const DEFAULT_IMG = {
-  default: '../images/dance.gif',
+  default: './images/dance.gif',
   typescript: 'https://www.typescriptlang.org/static/typescript-jp-bed04107658e1301111868439cd400a5.jpg',
   react: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
 }
